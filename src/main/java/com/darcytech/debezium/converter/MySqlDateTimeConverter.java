752442005fb4e2553ed76a11b76c8948b54ca3eb
liwei <liwei@git.com>
2023-04-13 10:34:16 +0800

    Upgrade to debezium 2.1

--- src/main/java/com/darcytech/debezium/converter/MySqlDateTimeConverter.java
+++ src/main/java/com/darcytech/debezium/converter/MySqlDateTimeConverter.java
@@ -7,6 +7,9 @@ import org.apache.kafka.connect.data.SchemaBuilder;
 
 import java.time.*;
 import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.Properties;
 import java.util.function.Consumer;
 
@@ -23,6 +26,7 @@ import java.util.function.Consumer;
  * | Datetime<br/>(2021-01-28 17:29:04)  | LocalDateTime<br/>(2021-01-28T17:29:04)  | Long<br/>(1611854944000)          |
  *
  * @see io.debezium.connector.mysql.converters.TinyIntOneToBooleanConverter
+ * 源代码地址: https://github.com/debezium/debezium/tree/1.9/debezium-connector-mysql/src/main/java/io/debezium/connector/mysql
  */
 @Slf4j
 public class MySqlDateTimeConverter implements CustomConverter<SchemaBuilder, RelationalColumn> {
@@ -33,6 +37,10 @@ public class MySqlDateTimeConverter implements CustomConverter<SchemaBuilder, Re
     private DateTimeFormatter timestampFormatter = DateTimeFormatter.ISO_DATE_TIME;
 
     private ZoneId timestampZoneId = ZoneId.systemDefault();
+    private static final String DEFAULT_DATE = "yyyy-MM-dd";
+    private static final String DEFAULT_TIME = "HH:mm:ss";
+    private static final String DEFAULT_DATETIME = "yyyy-MM-dd HH:mm:ss";
+    private static final String DEFAULT_TIMESTAMP_ZONE = "UTC+8";
 
     @Override
     public void configure(Properties props) {
@@ -46,44 +54,59 @@ public class MySqlDateTimeConverter implements CustomConverter<SchemaBuilder, Re
     private void readProps(Properties properties, String settingKey, Consumer<String> callback) {
         String settingValue = (String) properties.get(settingKey);
         if (settingValue == null || settingValue.length() == 0) {
-            return;
+            if (settingKey.equals("format.date")) {
+                settingValue = DEFAULT_DATE;
+            } else if (settingKey.equals("format.time")) {
+                settingValue = DEFAULT_TIME;
+            } else if (settingKey.equals("format.datetime")) {
+                settingValue = DEFAULT_DATETIME;
+            } else if (settingKey.equals("format.timestamp")) {
+                settingValue = DEFAULT_DATETIME;
+            } else if (settingKey.equals("format.timestamp.zone")) {
+                settingValue = DEFAULT_TIMESTAMP_ZONE;
+            } else {
+                return;
+            }
         }
         try {
             callback.accept(settingValue.trim());
         } catch (IllegalArgumentException | DateTimeException e) {
-            log.error("The \"{}\" setting is illegal:{}", settingKey, settingValue);
+            log.error("MySqlDateTimeConverter The \"{}\" setting is illegal:{}", settingKey, settingValue);
             throw e;
         }
     }
 
     @Override
     public void converterFor(RelationalColumn column, ConverterRegistration<SchemaBuilder> registration) {
-        String sqlType = column.typeName().toUpperCase();
+        String sqlType = column.typeName();
         SchemaBuilder schemaBuilder = null;
         Converter converter = null;
-        if ("DATE".equals(sqlType)) {
+        if ("DATE".equalsIgnoreCase(sqlType)) {
             schemaBuilder = SchemaBuilder.string().optional().name("com.darcytech.debezium.date.string");
             converter = this::convertDate;
         }
-        if ("TIME".equals(sqlType)) {
+        if ("TIME".equalsIgnoreCase(sqlType)) {
             schemaBuilder = SchemaBuilder.string().optional().name("com.darcytech.debezium.time.string");
             converter = this::convertTime;
         }
-        if ("DATETIME".equals(sqlType)) {
+        if ("DATETIME".equalsIgnoreCase(sqlType)) {
             schemaBuilder = SchemaBuilder.string().optional().name("com.darcytech.debezium.datetime.string");
             converter = this::convertDateTime;
         }
-        if ("TIMESTAMP".equals(sqlType)) {
+        if ("TIMESTAMP".equalsIgnoreCase(sqlType)) {
             schemaBuilder = SchemaBuilder.string().optional().name("com.darcytech.debezium.timestamp.string");
             converter = this::convertTimestamp;
         }
         if (schemaBuilder != null) {
             registration.register(schemaBuilder, converter);
-            log.info("register converter for sqlType {} to schema {}", sqlType, schemaBuilder.name());
+            log.info("MySqlDateTimeConverter register converter for sqlType {} to schema {}", sqlType, schemaBuilder.name());
         }
     }
 
     private String convertDate(Object input) {
+        if (Objects.isNull(input)) {
+            return null;
+        }
         if (input instanceof LocalDate) {
             return dateFormatter.format((LocalDate) input);
         }
@@ -91,10 +114,14 @@ public class MySqlDateTimeConverter implements CustomConverter<SchemaBuilder, Re
             LocalDate date = LocalDate.ofEpochDay((Integer) input);
             return dateFormatter.format(date);
         }
-        return null;
+        log.error("MySqlDateTimeConverter convertDate is fail getClass:{}、value:{}", input.getClass(), input.toString());
+        return Optional.ofNullable(input).map(p -> p.toString()).orElse(null);
     }
 
     private String convertTime(Object input) {
+        if (Objects.isNull(input)) {
+            return null;
+        }
         if (input instanceof Duration) {
             Duration duration = (Duration) input;
             long seconds = duration.getSeconds();
@@ -102,24 +129,52 @@ public class MySqlDateTimeConverter implements CustomConverter<SchemaBuilder, Re
             LocalTime time = LocalTime.ofSecondOfDay(seconds).withNano(nano);
             return timeFormatter.format(time);
         }
-        return null;
+        log.error("MySqlDateTimeConverter convertTime is fail getClass:{}、value:{}", input.getClass(), input.toString());
+        return Optional.ofNullable(input).map(p -> p.toString()).orElse(null);
     }
 
     private String convertDateTime(Object input) {
+        if (Objects.isNull(input)) {
+            return null;
+        }
         if (input instanceof LocalDateTime) {
             return datetimeFormatter.format((LocalDateTime) input);
         }
-        return null;
+        if (input instanceof java.sql.Timestamp) {
+            Date date = (java.sql.Timestamp) input;
+            LocalDateTime localDateTime = dateToLocalDateTime(date);
+            return datetimeFormatter.format(localDateTime);
+        }
+        log.error("MySqlDateTimeConverter convertDateTime is fail getClass:{}、value:{}", input.getClass(), input.toString());
+        return Optional.ofNullable(input).map(p -> p.toString()).orElse(null);
     }
 
     private String convertTimestamp(Object input) {
+        if (Objects.isNull(input)) {
+            return null;
+        }
         if (input instanceof ZonedDateTime) {
             // mysql的timestamp会转成UTC存储，这里的zonedDatetime都是UTC时间
             ZonedDateTime zonedDateTime = (ZonedDateTime) input;
             LocalDateTime localDateTime = zonedDateTime.withZoneSameInstant(timestampZoneId).toLocalDateTime();
             return timestampFormatter.format(localDateTime);
         }
-        return null;
+        if (input instanceof java.sql.Timestamp) {
+            Date date = (java.sql.Timestamp) input;
+            LocalDateTime localDateTime = dateToLocalDateTime(date);
+            return timestampFormatter.format(localDateTime);
+        }
+        log.error("MySqlDateTimeConverter convertTimestamp is fail getClass:{}、value:{}", input.getClass(), input.toString());
+        return Optional.ofNullable(input).map(p -> p.toString()).orElse(null);
     }
 
+    /**
+     * 将 Date 转为 LocalDateTime
+     *
+     * @param date
+     * @return java.time.LocalDateTime;
+     */
+    public LocalDateTime dateToLocalDateTime(Date date) {
+        return date.toInstant().atZone(timestampZoneId).toLocalDateTime();
+    }
 }
